e7760c8f11ff78ceb20633b0d1fb4489f28761d6
Jon Sadowski <jwsadowski@gmail.com>
2023-01-03 16:06:03 -0600

    second commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
+const commentsRoutes = require('./comments-routes.js');
 
 router.use('/', homeRoutes)
+router.use('/comments-routes', commentsRoutes)
 
 module.exports = router;
